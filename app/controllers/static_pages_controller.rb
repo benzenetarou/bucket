96edbc65ea0595b93af244fba8bb844ee2b20aa6
benzenetarou <benzenetarou@gmail.com>
2017-07-15 10:53:34 +0900

    change number of bucket per page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,7 +3,7 @@ class StaticPagesController < ApplicationController
   def home
     if logged_in?
       @bucket = current_user.buckets.build if logged_in?
-      @feed_items = current_user.feed.paginate(page: params[:page])
+      @feed_items = current_user.feed.paginate(page: params[:page], :per_page => 100)
     end
   end
 
